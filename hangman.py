87091f27d0b36de4c2ab2541b84071cda74b9967
Eric Underwood <e.b.underwood@gmail.com>
2025-02-28 21:37:29 -0800

    Finished Hangman

--- hangman.py
+++ hangman.py
@@ -3,15 +3,65 @@
 # Date:  February 28, 2025
 
 import random
-import sys
 
 def print_progress(p):
     return_string = ""
     for item in p:
         return_string += item + " "
-    return(return_string)
+    return(return_string) 
 
-# TODO:  Randomly choose a word from world_list and assign it to a variable called chosen_word.  Print it.
+
+stages = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+========='''] 
+
 word_list = ['aardvark', 'babboon', 'camel']
 chosen_word = random.choice(word_list)
 
@@ -23,19 +73,24 @@ print(f"Chosen Word:  {chosen_word}")
 print(print_progress(progress))
 
 # Game Loop:  Get input, check input against word, print out status.
-while True:
+done = False
+wrong_guesses = 0
+print(stages[wrong_guesses])
+while not done and wrong_guesses <= 5:
     user_guess = input("Please enter your guess:  ").lower()
-    if not user_guess.isalpha():
-        print("Please enter letters only")
-        sys.exit(0)
-    print(f"User guessed {user_guess}")
 
     for i in range(len(chosen_word)):
         if user_guess == chosen_word[i]:
             progress[i] = user_guess
+    if user_guess not in chosen_word:
+        print(f"{user_guess} is not in the secret word")
+        wrong_guesses += 1
+        print(stages[wrong_guesses])
             
     if "_" not in progress:
-        print("You Win!")
-        sys.exit(0)
+        print(f"You Win!  You guessed '{chosen_word.upper()}'")
+        done = True
     else:
-        print(print_progress(progress))
+        print(print_progress(progress)+"\n")
+        if wrong_guesses > 5:
+            print("No more guesses.  You lose!")
