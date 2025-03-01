5c2ea042dd52f81f700d086756f6b2d39569df57
Eric Underwood <e.b.underwood@gmail.com>
2025-03-01 14:48:37 -0800

    Caesar Cipher progress

---
+++ caesar_ciper.py
@@ -0,0 +1,44 @@
+# Program name:  Caesar Cipher
+# Programmer:  Eric Underwood 
+# Date:  March 1, 2025
+
+game_over = False
+intro_graphic = '''
+
+            ('-.       ('-.    .-')     ('-.     _  .-')                              _ (`-.  ('-. .-.   ('-.  _  .-')   
+            ( OO ).-. _(  OO)  ( OO ).  ( OO ).-.( \( -O )                            ( (OO  )( OO )  / _(  OO)( \( -O )  
+   .-----.  / . --. /(,------.(_)---\_) / . --. / ,------.          .-----.  ,-.-')  _.`     \,--. ,--.(,------.,------.  
+  '  .--./  | \-.  \  |  .---'/    _ |  | \-.  \  |   /`. '        '  .--./  |  |OO)(__...--''|  | |  | |  .---'|   /`. ' 
+  |  |('-..-'-'  |  | |  |    \  :` `..-'-'  |  | |  /  | |        |  |('-.  |  |  \ |  /  | ||   .|  | |  |    |  /  | | 
+ /_) |OO  )\| |_.'  |(|  '--.  '..`''.)\| |_.'  | |  |_.' |       /_) |OO  ) |  |(_/ |  |_.' ||       |(|  '--. |  |_.' | 
+ ||  |`-'|  |  .-.  | |  .--' .-._)   \ |  .-.  | |  .  '.'       ||  |`-'| ,|  |_.' |  .___.'|  .-.  | |  .--' |  .  '.' 
+(_'  '--'\  |  | |  | |  `---.\       / |  | |  | |  |\  \       (_'  '--'\(_|  |    |  |     |  | |  | |  `---.|  |\  \  
+   `-----'  `--' `--' `------' `-----'  `--' `--' `--' '--'         `-----'  `--'    `--'     `--' `--' `------'`--' '--' '''
+
+
+def caesar(message, direction, shift):
+    if direction == 'decode':
+        shift *= -1  
+    result = ""
+    for letter in message:
+        if not letter.isalpha():
+            result += letter
+        else:
+            index = alphabet.index(letter) + shift
+            index %= len(alphabet)
+            result += alphabet[index]
+    print(f"Here's the {direction}d result:  {result}") 
+
+print(intro_graphic) 
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i","j","k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+while not game_over:
+    type = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n") 
+    message = input("Type your message:\n")
+    shift = int(input("Type your shift number: \n"))  
+
+    caesar(message, type, shift)
+
+    if input("Type 'yes' if you want to go again.  Otherwise type 'no'.\n") == "no":
+        game_over = True
--- funcs.py
+++ funcs.py
@@ -11,4 +11,20 @@ while x > 0:
     print(x)
     x -= 1
 
-    
+# Create function called Greet(), write 3 print statements inside of greet, then call greet.
+def greet(something):
+    print(f"Hello, {something}")
+    print("This is the greet function")
+    print("Good bye") 
+
+greet("Eric")
+
+
+def keyword_arguments(first, second, third): 
+    print(first) 
+    print(second) 
+    print(third)
+
+keyword_arguments(third = "third", first = "first", second = "second") 
+
+
