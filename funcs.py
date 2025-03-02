b1cd9688ddd514182f55b20650b327a2cbe832a0
Eric Underwood <e.b.underwood@gmail.com>
2025-03-02 13:10:05 -0800

    Initialize Calculator

--- funcs.py
+++ funcs.py
@@ -12,7 +12,8 @@ while x > 0:
     x -= 1
 
 # Create function called Greet(), write 3 print statements inside of greet, then call greet.
-def greet(something):
+def greet(something): 
+    """This function says hello"""   #This is a docstring.
     print(f"Hello, {something}")
     print("This is the greet function")
     print("Good bye") 
@@ -25,6 +26,13 @@ def keyword_arguments(first, second, third):
     print(second) 
     print(third)
 
-keyword_arguments(third = "third", first = "first", second = "second") 
+keyword_arguments(third = "third", first = "first", second = "second")  
+
+# Take two inputs returns title case single string
+def format_name(f_name, l_name):
+    return(f"{f_name.title()} {l_name.title()}")
+
+print(f"Format Name:  {format_name("eric", "underwood")}")
+
 
 
