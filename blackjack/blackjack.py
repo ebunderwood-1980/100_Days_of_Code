b6c84a418b0ba4174ac4ba0d4f2b858f5a5ab6f2
Eric Underwood <e.b.underwood@gmail.com>
2025-03-10 21:50:02 -0700

    Blackjack rewrite

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,39 +1,39 @@
-# Programmer:  Eric Underwood
-# Program:  Blackjack
-# Date:  March 3, 2025 
-
+from constants import DECK
+from constants import LOGO
 import random
-from logo import logo
-from logo import deck
 
-# Function Definitions
-def main():
-    # Variable Definitions
-    game_over = False 
-    player_hand = [] 
-    computer_hand = []
-    computer_hidden_card = None
-    
-    # Print out the Blackjack logo
-    print(logo)
 
-    # Game loop
-    while not game_over:
-        # Deal 2 random cards to the user and 1 random card to the computer
-        # with another face down random card dealt to the computer as well
-        for _ in range(2):
-            player_hand.append(random.choice(list(deck.keys()))) 
-        computer_hand.append(random.choice(list(deck.keys()))) 
-        computer_hidden_card = random.choice(list(deck.keys()))
+# Variables
+user_hand = []
+computer_hand = [] 
+game_over = False
+user_turn = True
 
-        # Print out both user and computer hands
-        print(f"User Hand: {player_hand}")
-        print(f"Computer Hand:  {computer_hand}") 
-        print(f"Computer Hidden Card:  {computer_hidden_card}")
+print(LOGO)
 
+for _ in range(2):
+    user_hand.append(random.choice(list(DECK.keys())))
+    computer_hand.append(random.choice(list(DECK.keys())))
+
+print(f"User hand:  {user_hand}")
+print(f"Computer hand:  {computer_hand}")
+
+if computer_hand[0] == 'A':
+    print("Ace showing, checking Dealer's Hole Card...") 
+    if DECK[computer_hand[1]][0] == 10: 
+        print("Dealer hits Blackjack!")
         game_over = True
+    else:
+        print("No dice, lucky break")
+if computer_hand[0] in ['K', 'Q', 'J', '10']:
+    print("Face card showing, checking Dealer's Hole Card...")
+    if computer_hand[1] == 'A':
+        print("Dealer hits Blackjack!")
+        game_over = True
+    else:
+        print("No dice, lucky break")
 
+while user_turn:
+    take_hit = input("Type 'y' to get another card, type 'n' to pass:  ")
 
-
-if __name__ == '__main__':
-    main()
+    
